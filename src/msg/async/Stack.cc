9e70f213750b2caa4481ff13675dd939c876549d
root <root@rhs-cli-08.gdev.lab.eng.rdu2.redhat.com>
2016-11-07 11:47:38 -0500

    rebase
    Signed-off-by: root <root@rhs-cli-08.gdev.lab.eng.rdu2.redhat.com>

--- src/msg/async/Stack.cc
+++ src/msg/async/Stack.cc
@@ -14,6 +14,7 @@
  *
  */
 
+#include <urcu.h>
 #include "common/Cond.h"
 #include "common/errno.h"
 #include "PosixStack.h"
@@ -38,6 +39,8 @@ void NetworkStack::add_thread(unsigned i, std::function<void ()> &thread)
       ldout(cct, 10) << __func__ << " starting" << dendl;
       w->initialize();
       w->init_done();
+      pthread_setspecific(w->cct->registered, this);
+      rcu_register_thread();
       while (!w->done) {
         ldout(cct, 30) << __func__ << " calling event process" << dendl;
 
@@ -48,6 +51,7 @@ void NetworkStack::add_thread(unsigned i, std::function<void ()> &thread)
           // TODO do something?
         }
       }
+      rcu_unregister_thread();
       w->reset();
     }
   );
